1a9b6137a5db6f5e0f20748c5d4a6e70c9c25f96
Kirill Zorin <zarincheg@gmail.com>
2017-03-27 18:17:59 +0300

    Refactor to use new version of the dialogs lib

--- web/simplechat.php
+++ web/simplechat.php
@@ -11,21 +11,14 @@ include "../vendor/autoload.php";
 use ExampleDialogs\HelloDialog;
 use josegonzalez\Dotenv\Loader;
 use Telegram\Bot\Api;
-use Illuminate\Redis\RedisManager as Redis;
+use Predis\Client as Redis;
 use BotDialogs\Dialogs;
 
 $dotenvLoader = new Loader('../.env');
 $dotenvLoader->parse()->putenv(true);
 
 $telegram = new Api(getenv("TELEGRAM_TOKEN"));
-$redis = new Redis('predis', [
-    'cluster' => false,
-    'default' => [
-        'host' => '127.0.0.1',
-        'port' => '6379',
-        'database' => 0
-    ]
-]);
+$redis = new Redis('tcp://127.0.0.1:6379');
 $dialogs = new Dialogs($telegram, $redis);
 $update = $telegram->getWebhookUpdates();
 $dialogs->add(new HelloDialog($update));
